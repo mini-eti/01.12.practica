e8c49c8574b7732e464c9eaf28f94cc33000ae2a
root <mansurov.artem07@gmail.com>
2025-12-01 17:35:53 +0300

    canvas

--- main0112.cpp
+++ main0112.cpp
@@ -27,19 +27,49 @@ namespace top{
     p_t o;
     Dot(int x, int y);
 
-  }
+  };
 
+
+  struct frame_t{
+    p_t left_bot;
+    p_t right_top;
+  };
+
+  void make_f(IDraw** b, size_t k);
+
+  void get_points(IDraw* b, p_t** ps, size_t & s);
+
+  frame_t build_frame(const p_t*ps, size_t s);
+
+  char* build_convas(frame_t f);
 }
 
+int main(){
+  using namespace top;
+  IDraw* f[3] = {};
+  p_t* p = nullptr;
+  size_t s = 0;
+  try{
+    make_f(f,3);
+    for(size_t i = 0; i < 3; ++i){
+      get_points(f[i], &p, s);
+    }
+    frame_t fr = build_frame(p, s);
+    char * cnv = build_convas(fr);
+  }
+  catch(...){
+
+  }
+}
 top::Dot::Dot(int x, int y):
   IDraw(),
   o{x,y}
   {}
 
-top::p_t top::Dot::begin(){
+top::p_t top::Dot::begin()const{
   return o;
 }
 
-top::p_t top::Dot::next(p_t){
+top::p_t top::Dot::next(p_t)const{
   return begin();
 }
