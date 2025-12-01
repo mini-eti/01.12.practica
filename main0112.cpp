25058b9bb07b2282114702f8a4a16b84e7394eaf
root <mansurov.artem07@gmail.com>
2025-12-01 17:00:12 +0300

    initial commit

---
+++ main0112.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+
+struct p_t
+{
+  int x;
+  int y;
+};
+
+
+ struct IDraw
+ {  
+  virtual p_t begin() = 0;
+  virtual p_t next(p_t) = 0;
+ };
+ 
+bool operator==(p_t a, p_t b){
+  return a.x == b.x && a.y == b.y;
+}
+
+bool operator!=(p_t a, p_t b){
+  return !(a==b);
+}
