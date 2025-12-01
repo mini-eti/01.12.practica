8aa40a0f22a88d9ef27e29dc9a3ee61d17704937
root <mansurov.artem07@gmail.com>
2025-12-01 17:17:17 +0300

    with dot

--- main0112.cpp
+++ main0112.cpp
@@ -1,22 +1,45 @@
 #include <iostream>
+namespace top{
+  struct p_t
+  {
+    int x;
+    int y;
+  };
 
-struct p_t
-{
-  int x;
-  int y;
-};
 
+  struct IDraw
+  {  
+    virtual p_t begin() const = 0 ;
+    virtual p_t next(p_t) const = 0;
+  };
+  
+  bool operator==(p_t a, p_t b){
+    return a.x == b.x && a.y == b.y;
+  }
 
- struct IDraw
- {  
-  virtual p_t begin() = 0;
-  virtual p_t next(p_t) = 0;
- };
- 
-bool operator==(p_t a, p_t b){
-  return a.x == b.x && a.y == b.y;
+  bool operator!=(p_t a, p_t b){
+    return !(a==b);
+  }
+
+  struct Dot:IDraw{
+    p_t begin() const override;
+    p_t next(p_t) const override;
+    p_t o;
+    Dot(int x, int y);
+
+  }
+
 }
 
-bool operator!=(p_t a, p_t b){
-  return !(a==b);
+top::Dot::Dot(int x, int y):
+  IDraw(),
+  o{x,y}
+  {}
+
+top::p_t top::Dot::begin(){
+  return o;
+}
+
+top::p_t top::Dot::next(p_t){
+  return begin();
 }
